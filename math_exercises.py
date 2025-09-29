5206b02eb1b9736e0e5da3674a87d1ceca0a787a
MartinN <martin.nugis@voco.ee>
2025-09-29 10:51:59 +0300

    Ülesanne: Matemaatilised avaldised

---
+++ math_exercises.py
@@ -0,0 +1,46 @@
+#Sum ja difference
+num_a = 5
+num_b = 2
+sum = num_a + num_b
+difference = num_a - num_b
+print(sum)
+
+#float division
+
+division = num_a / num_b
+print(division)
+
+#integer division
+division = num_a // num_b
+print
+
+#Powerful operations
+multiply_numbers = num_a * num_b
+power = num_a ** num_b
+remainder = num_a % num_b
+
+print(multiply_numbers)
+print(power)
+print(remainder)
+# average
+average = (num_a + num_b) / 2
+
+#area of circle
+answer = math.pi * (radius * radius)
+    circle_area = round(answer, 2)
+
+#Area of an equilateral triangle
+answer2 = math.sqrt(3) / 4 * side_lenght ** 2
+triangle_area = round(answer, 2)
+
+
+#Calculate discriminant
+discriminant = b ** 2 - 4 * a * c
+
+#Calculate hypotenuse lenght
+c = math.sqrt(a ** 2 + b ** 2)
+
+#Calculate cathetus lenght
+
+b = math.sqrt(c ** 2 - a ** 2)
+
