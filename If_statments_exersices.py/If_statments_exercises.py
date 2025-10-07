51b0820f5316c09ac6bac8eedc02ac7dfe85a4a7
MartinN <martin.nugis@voco.ee>
2025-10-07 11:44:56 +0300

    Ülesanne: Tingimuslause

---
+++ If_statments_exersices.py/If_statments_exercises.py
@@ -0,0 +1,18 @@
+#are equal
+def are_equal(num_a: int, num_b: int) -> str:
+    if num_a == num_b:
+        return "equal"
+    else:
+        return "not equal"
+
+#Positive or negative
+def positive_or_negative(num_a: int) -> str:
+    if positive_or_negative > 0:
+        return "positive"
+    if positive_or_negative < 0:
+        return "negative"
+    if positive_or_negative = 0:
+        return "zero"
+
+positive_or_negative() 
+        
