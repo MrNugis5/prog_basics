3c70fcbda9a9a82d5ff89901c9d8b4855409ebef
MartinN <martin.nugis@voco.ee>
2025-10-23 11:01:27 +0300

    Ülesanne: Järjend-moodle

--- list_exersice.py
+++ list_exersice.py
@@ -1,57 +1,108 @@
+"""Basic list operations."""
+
+
 def create_list_of_two_elements(a: int, b: int) -> list:
+    """
+    Return list of two elements a and b (in that order).
+
+    create_list_of_two_elements(1, 2) => [1, 2]
+    create_list_of_two_elements(81, 72) => [81, 72]
+    """
     elements = [a, b]
     return elements
 
 
-
-create_list_of_two_elements(81, 72)
-
 def create_list_with_edge_elements(elements: list) -> list:
-    if len(elements) ==  1:
+    """
+    Create a new list with 2 elements: the first and the last element.
+
+    The initial list has at least one element.
+    If the initial list has only one element, the same element is
+    both the first and the last element.
+
+    create_list_with_edge_elements([1, 2, 3]) => [1, 3]
+    create_list_with_edge_elements([1]) => [1, 1]
+    create_list_with_edge_elements(["a", "b"]) => ["a", "b"]
+    create_list_with_edge_elements(["a", "b", "c", "d", "f"]) => ["a", "f"]
+    """
+    if len(elements) == 1:
         return [elements[0], elements[0]]
     else:
         return [elements[0], elements[-1]]
 
 
-create_list_with_edge_elements([1, 2, 3])
-
 def get_middle_element_of_list(elements: list) -> any:
+    """
+    Return the middle element in the list with odd number of element.
+
+    The list always has odd number of elements.
+    get_middle_element_of_list([1]) = 1
+    get_middle_element_of_list([1, 2, 3]) => 2
+    get_middle_element_of_list(["a", "b", "c"]) => "b"
+    """
     if len(elements) == 1:
         return elements[0]
-    for num in elements:
-        middle = num // 2
-        return elements[middle]
-    
+    lenght = len(elements)
+    middle = lenght // 2
+    return elements[middle]
 
-get_middle_element_of_list([1, 2, 3])
 
 def get_middle_part_of_list(elements: list) -> list:
+    """
+    Create a new list of the input list where the first and the last element are removed.
 
-    
+    The initial list has at least 2 elements (don't have to validate that).
+
+    get_middle_part_of_list([1, 2, 3]) => [2]
+    get_middle_part_of_list([1, 3]) => []
+    get_middle_part_of_list([1, 3, 6, 7]) => [3, 6]
+    get_middle_part_of_list(["a", "b", "b", "a"]) => ["b", "b"]
+    """
     elements.pop(0), elements.pop(-1)
     return elements
 
 
-get_middle_part_of_list([1, 2, 3])
-
 def create_new_list_with_added_number(numbers: list, number: int) -> list:
-    new_list = [numbers, number]
-    return new_list
+    """
+    Return a new list where a number has been added.
 
-create_new_list_with_added_number([1, 2, 3], 4)
+    Do not modify the existing list.
+    create_new_list_with_added_number([1, 2, 3], 4) => [1, 2, 3, 4]
+    """
+    return numbers + [number]
+
 
 def swap_edge_elements(elements: list) -> list:
+    """
+    Swap the first and the last element.
+
+    List always has at least 2 elements.
+    Elements can be either numbers or strings.
+
+    swap_edge_elements([1, 2, 3]) => [3, 2, 1]
+    swap_edge_elements([1, 2]) => [2, 1]
+    swap_edge_elements([1, 2, 1, 4]) => [4, 2, 1, 1]
+    swap_edge_elements(["foo", "bar", "pub"]) => ["pub", "bar", "foo"]
+
+    """
     elements[0], elements[-1] = elements[-1], elements[0]
     return elements
 
-swap_edge_elements([1, 2])
 
 def add_element_in_position(elements: list, new_element: any, position: int) -> list:
+    """
+    Add a new element into the list into the specified position.
+
+    The position is always valid.
+    The elements on the position and on the right are shifted by one.
+
+    add_element_in_position([1, 2, 3], 2, 2) => [1, 2, 2, 3]
+    add_element_in_position([1], 9, 0) => [9, 1]
+    add_element_in_position([1], 9, 1) => [1, 9]
+    """
     elements.insert(position, new_element)
     return elements
 
-
-add_element_in_position([1, 2, 3], 2, 2)
 
 def get_repeated_list(elements: list, repetiton: int) -> list:
     """
@@ -61,10 +112,9 @@ def get_repeated_list(elements: list, repetiton: int) -> list:
     get_repeated_list([1], 5) => [1, 1, 1, 1, 1]
     get_repeated_list([1, 2], 0) => []
     """
-    [elements] * repetiton
+    elements = elements * repetiton
     return elements
 
-get_repeated_list([1, 2], 2)
 
 def remove_first_element_from_list(elements: list) -> None:
     """
@@ -78,5 +128,13 @@ def remove_first_element_from_list(elements: list) -> None:
     x => [2, 3]
     """
     elements.pop(0)
-    
-remove_first_element_from_list([1])
+
+
+def reverse_list(elements: list) -> list:
+    """
+    Return reversed list.
+
+    reverse_list([1, 2, 3]) => [3, 2, 1]
+    reverse_list(["a", "b"]) => ["b", "a"]
+    """
+    return elements[::-1]
