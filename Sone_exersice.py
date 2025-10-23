952f354fb0db0f4177b354c1d8fa72a9bfb515c8
MartinN <martin.nugis@voco.ee>
2025-10-23 08:37:56 +0300

    Ülesanne: Sõne

---
+++ Sone_exersice.py
@@ -0,0 +1,8 @@
+cake = "vahukoor\nmarjad\ntäidis\npõhi"
+print(cake)
+original_string = "Programming is fun!"
+backward = original_string[::-1]
+every_other = original_string[::2]
+first_word_reversed = original_string[0:11::-1]
+print(first_word_reversed)
+
