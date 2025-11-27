09ea02f12e1c350d7098ac0221545df19cf4af30
MrNugis5 <martin.nugis@voco.ee>
2025-11-27 11:36:47 +0200

    Create exersice_testing.py

---
+++ exersice_testing.py
@@ -0,0 +1,50 @@
+"""Solutions to be tested."""
+
+
+def students_study(time: int, coffee_needed: bool) -> bool:
+    """
+    Return True if students study in given circumstances.
+
+    (19, False) -> True
+    (1, True) -> False.
+    """
+    if time >= 18:
+        return True
+    if time >= 5 and coffee_needed:
+        return True
+    if time >= 1:
+        return False
+
+
+def lottery(a: int, b: int, c: int) -> int:
+    """
+    Return Lottery victory result 10, 5, 1, or 0 according to input values.
+
+    (5, 5, 5) -> 10
+    (2, 2, 1) -> 0
+    (2, 3, 1) -> 1
+    """
+    if a == 5 and b == 5 and c == 5:
+        return 10
+    if a == b and a == c:
+        return 5
+    if b != a and c != a:
+        return 1
+    if b == a or c == a:
+        return 0
+
+
+def fruit_order(small_baskets: int, big_baskets: int, ordered_amount: int) -> int:
+    """
+    Return number of small fruit baskets if it's possible to finish the order, otherwise return -1.
+
+    (4, 1, 9) -> 4
+    (3, 1, 10) -> -1
+    """
+    big_size = 5
+    max_big_used = min(big_baskets, ordered_amount // big_size)
+    remaining = ordered_amount - max_big_used * big_size
+    if remaining <= small_baskets:
+        return remaining
+    else:
+        return -1
